b9f8fcb354e4f465f0d3224d980edecdab5efe20
Marianna de Oliveira <marianna.oad@hotmail.com>
2022-04-12 17:41:54 -0300

    finally free
    for now

--- js/script.js
+++ js/script.js
@@ -4,3 +4,26 @@ const navbar = document.querySelector('.menu');
 menuIcon.addEventListener('click', () => {
     navbar.classList.toggle("change");
 });
+
+
+// function validarFormContato(){
+//     let nome = formContato.name.value
+//     let email = formContato.email.value
+//     let mensgem = formContato.mensagem.value
+
+//     if (nome === ''){
+//         alert('O campo Nome é obrigatório')
+//         formContato.nome.focus()
+//         return false
+//     }
+
+//     if (email === ''){
+//         alert('O campo E-mail é obrigatório')
+//         formContato.email.focus()
+//         return false
+//     }
+// }
+
+document.querySelector("form").addEventListener("submit", event => {
+    console.log('enviar formu')
+})
